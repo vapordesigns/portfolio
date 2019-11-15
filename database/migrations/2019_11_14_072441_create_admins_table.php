24070c57e69491faeaf2b84ed45f3b151a10dccc
Steven Nyanumba <stevennmb9@gmail.com>
2019-11-15 13:38:09 +0300

    Added Admins Table and Blade Panels

--- database/migrations/2019_11_14_072441_create_admins_table.php
+++ database/migrations/2019_11_14_072441_create_admins_table.php
@@ -15,6 +15,7 @@ class CreateAdminsTable extends Migration
     {
         Schema::create('admins', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->integer('user_id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
